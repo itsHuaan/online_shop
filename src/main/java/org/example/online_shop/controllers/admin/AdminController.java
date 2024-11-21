9ee7ad176f8d949ae0250250559ee45486c7e4c8
itsHuaan <doquanghuan1909@gmail.com>
2024-11-22 00:54:52 +0700

    just add something new to the admin panel

--- src/main/java/org/example/online_shop/controllers/admin/AdminController.java
+++ src/main/java/org/example/online_shop/controllers/admin/AdminController.java
@@ -1,7 +1,8 @@
 package org.example.online_shop.controllers.admin;
 
-import org.example.online_shop.models.ProductModel;
+import org.example.online_shop.models.*;
 import org.example.online_shop.services.*;
+import org.example.online_shop.services.impl.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,14 +17,16 @@ public class AdminController {
     private final ICategoryService categoryService;
     private final IAuthorService authorService;
     private final IDiscountService discountService;
+    private final IPostService postService;
 
     @Autowired
-    public AdminController(IProductService productService, IUserService userService, ICategoryService categoryService, IAuthorService authorService, IDiscountService discountService) {
+    public AdminController(IProductService productService, IUserService userService, ICategoryService categoryService, IAuthorService authorService, IDiscountService discountService, IPostService postService) {
         this.productService = productService;
         this.userService = userService;
         this.categoryService = categoryService;
         this.authorService = authorService;
         this.discountService = discountService;
+        this.postService = postService;
     }
 
     @GetMapping
@@ -57,8 +60,8 @@ public class AdminController {
 
     @PostMapping("/create-product")
     public String createProduct(Model model, @ModelAttribute("ProductModel") ProductModel productModel, RedirectAttributes attributes) {
-        int ressult = productService.save(productModel);
-        if (ressult == 1) {
+        int result = productService.save(productModel);
+        if (result == 1) {
             attributes.addFlashAttribute("success", "Thêm  thành công");
         }else {
             attributes.addFlashAttribute("error", "Thêm thất bại");
@@ -66,12 +69,34 @@ public class AdminController {
         return "redirect:/admin/products";
     }
 
+    @PostMapping("/create-post")
+    public String createPost(Model model, @ModelAttribute("postModel") PostModel postModel, RedirectAttributes attributes) {
+        int result = postService.save(postModel);
+        if (result == 1) {
+            attributes.addFlashAttribute("success", "Thêm thành công");
+        }else {
+            attributes.addFlashAttribute("error", "Thêm thất bại");
+        }
+        return "redirect:/admin/posts";
+    }
+
+    @GetMapping("/delete-post/{id}")
+    public String deletePost(Model model, RedirectAttributes attributes, @PathVariable Long id){
+        int result = postService.delete(id);
+        if (result == 1) {
+            attributes.addFlashAttribute("success", "Xóa thành công");
+        }else {
+            attributes.addFlashAttribute("error", "Xóa thất bại");
+        }
+        return "redirect:/admin/posts";
+    }
+
     @PostMapping("/update-product/{id}")
     public String updateProduct(Model model, @ModelAttribute("ProductModel") ProductModel productModel, RedirectAttributes attributes, @PathVariable Long id)  {
         productModel.setProductId(id);
-        int ressult = productService.save(productModel);
-        if (ressult == 2) {
-            attributes.addFlashAttribute("success", "Cập nhật  thành công");
+        int result = productService.save(productModel);
+        if (result == 2) {
+            attributes.addFlashAttribute("success", "Cập nhật thành công");
         }else {
             attributes.addFlashAttribute("error", "Cập nhật thất bại");
         }
@@ -79,10 +104,10 @@ public class AdminController {
     }
 
     @GetMapping("/delete-product/{id}")
-    public String updateProduct(Model model, RedirectAttributes attributes, @PathVariable Long id){
-        int ressult = productService.delete(id);
-        if (ressult == 1) {
-            attributes.addFlashAttribute("success", "Xóa  thành công");
+    public String deleteProduct(Model model, RedirectAttributes attributes, @PathVariable Long id){
+        int result = productService.delete(id);
+        if (result == 1) {
+            attributes.addFlashAttribute("success", "Xóa thành công");
         }else {
             attributes.addFlashAttribute("error", "Xóa thất bại");
         }
@@ -105,13 +130,48 @@ public class AdminController {
 
     @GetMapping("/authors")
     public String listAuthors(Model model){
+        model.addAttribute("authorModel", new AuthorModel());
         model.addAttribute("authors", authorService.findAll());
         model.addAttribute("currentPath", "/authors");
         return "admin/author/authors";
     }
 
+    @PostMapping("/add-author")
+    public String addAuthor(Model model, @ModelAttribute("authorModel") AuthorModel authorModel, RedirectAttributes attributes) {
+        int result = authorService.save(authorModel);
+        if (result == 1) {
+            attributes.addFlashAttribute("success", "Thêm thành công");
+        }else {
+            attributes.addFlashAttribute("error", "Thêm thất bại");
+        }
+        return "redirect:/admin/authors";
+    }
+
+    @PostMapping("/add-category")
+    public String addCategory(Model model, @ModelAttribute("categoryModel") CategoryModel categoryModel, RedirectAttributes attributes) {
+        int result = categoryService.save(categoryModel);
+        if (result == 1) {
+            attributes.addFlashAttribute("success", "Thêm thành công");
+        }else {
+            attributes.addFlashAttribute("error", "Thêm thất bại");
+        }
+        return "redirect:/admin/categories";
+    }
+
+    @PostMapping("/add-discount")
+    public String addDiscount(Model model, @ModelAttribute("discountModel") DiscountModel discountModel, RedirectAttributes attributes) {
+        int result = discountService.save(discountModel);
+        if (result == 1) {
+            attributes.addFlashAttribute("success", "Thêm thành công");
+        }else {
+            attributes.addFlashAttribute("error", "Thêm thất bại");
+        }
+        return "redirect:/admin/discounts";
+    }
+
     @GetMapping("/categories")
     public String listCategories(Model model){
+        model.addAttribute("categoryModel", new CategoryModel());
         model.addAttribute("categories", categoryService.findAll());
         model.addAttribute("currentPath", "/categories");
         return "admin/category/categories";
@@ -119,9 +179,25 @@ public class AdminController {
 
     @GetMapping("/discounts")
     public String listPromotions(Model model){
+        model.addAttribute("discountModel", new DiscountModel());
         model.addAttribute("discounts", discountService.findAll());
         model.addAttribute("currentPath", "/discounts");
         return "admin/discount/discounts";
+    }
+
+
+    @GetMapping("/posts")
+    public String listPosts(Model model){
+        model.addAttribute("posts", postService.findAll());
+        model.addAttribute("currentPath", "/posts");
+        return "admin/post/posts";
+    }
+
+    @GetMapping("/add-posts")
+    public String addPost(Model model) {
+        model.addAttribute("postModel", new PostModel());
+        model.addAttribute("currentPath", "/add-posts");
+        return "admin/post/add-post";
     }
 
 }
--- src/main/resources/static/admin/static/js/pages/ckeditor.js
+++ src/main/resources/static/admin/static/js/pages/ckeditor.js
@@ -1,7 +1,18 @@
 ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
-  console.error(error)
+    console.error(error)
 })
 
 ClassicEditor.create(document.querySelector("#book-description")).catch((error) => {
-  console.error(error)
+    console.error(error)
 })
+
+ClassicEditor.create(document.querySelector("#post-content")).then(
+    editor => {
+        editor.ui.view.editable.element.style.minHeight = '400px';
+    }).catch((error) => {
+    console.error(error)
+})
+
+/*ClassicEditor.create(document.querySelector("#post-content")).catch((error) => {
+    console.error(error)
+})*/
