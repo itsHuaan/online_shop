9df99f51ee45a266e37450bc85f7a11ac22a3f9e
sonvt <vuthanhson041995@gmail.com>
2024-11-21 16:48:14 +0700

    register user

--- src/main/java/org/example/online_shop/controllers/customer/HomeController.java
+++ src/main/java/org/example/online_shop/controllers/customer/HomeController.java
@@ -1,19 +1,24 @@
 package org.example.online_shop.controllers.customer;
 
 import org.example.online_shop.dto.ProductDto;
+import org.example.online_shop.models.UserModel;
 import org.example.online_shop.services.IProductService;
+import org.example.online_shop.services.IUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 public class HomeController {
     private final IProductService productService;
-
-    public HomeController(IProductService productService) {
+    private final IUserService userService;
+    public HomeController(IProductService productService, IUserService userService) {
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
@@ -28,7 +33,20 @@ public class HomeController {
 
     @GetMapping("/register")
     public String register(Model model) {
+        UserModel userModel = new UserModel();
+        model.addAttribute("UserModel", userModel);
         return "customer/register";
+    }
+
+    @GetMapping("/signup")
+    public String signup(@ModelAttribute("UserModel") UserModel userModel, RedirectAttributes attributes) {
+        int result = userService.save(userModel);
+        if (result == 1){
+            attributes.addFlashAttribute("success", "Đăng ký thành công");
+        }else {
+            attributes.addFlashAttribute("error", "Đăng ký thất bại");
+        }
+        return "redirect:/login";
     }
 
     @GetMapping("/product")
@@ -37,4 +55,5 @@ public class HomeController {
         model.addAttribute("products", productDtos);
         return "customer/product";
     }
+
 }
